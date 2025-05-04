bb7e03d2d568e9203f2ba2a6b762eb6540937378
khushwant <awsmkhush@gmail.com>
2025-05-04 22:36:44 +0530

    Barebone of jokeGenerator

---
+++ src/main/java/com/khushwant/Joke_genrator/JokeGenratorApplication.java
@@ -0,0 +1,13 @@
+package com.khushwant.Joke_genrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeGenratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeGenratorApplication.class, args);
+	}
+
+}
